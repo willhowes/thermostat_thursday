e0b8437eb059309028d7c11b33424fca52ddc336
Will Howes <williameldenhowes@gmail.com>
2019-07-24 17:36:50 +0100

    Refactored

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ $( document ).ready(function() {
 
   $( "#up" ).click(function() {
   thermostat.up();
-  $("#temperature").html(thermostat.temperature)
+  // $("#temperature").html(thermostat.temperature)
   console.log(thermostat.temperature)
 });
 
-$( "#down" ).click(function() {
-thermostat.down();
-$("#temperature").html(thermostat.temperature)
-console.log(thermostat.temperature)
-});
+  $( "#down" ).click(function() {
+  thermostat.down();
+  // $("#temperature").html(thermostat.temperature)
+  console.log(thermostat.temperature)
+  });
 
-$("#temperature").html(thermostat.temperature)
+  $("button").click(function() {
+    $("#temperature").html(thermostat.temperature)
+  })
+
+  $("#temperature").html(thermostat.temperature)
 
 });
